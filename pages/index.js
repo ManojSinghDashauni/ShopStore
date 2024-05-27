352e6dc6cb748a4b4df08e6ea6b11cd634196203
ManojSinghDashauni <97912816+ManojSinghDashauni@users.noreply.github.com>
2024-05-27 22:36:31 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ const Home = ( {products, bannerData}) => (
       <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
       <div className="products-heading">
         <h2>Best selling Products</h2>
-        <p>Speakers of many variations</p>
+        <p>Product of many variations</p>
       </div>
       <div className="products-container">
         {products?.map((product)=> <Product key={product._id} product={product} />)}
